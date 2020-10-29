ad526652c9ade291c2f08570bd983c13df6f4735
Harritone <yourrasik@gmail.com>
2020-10-29 12:01:15 +1000

    Initial commit

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', '_YOU_USER_');
+  define('DB_PASS', '_PASSWORD_');
+  define('DB_NAME', '_DB_NAME_');
+
+// App root
+define('APPROOT', dirname(dirname(__FILE__)));
+// URL Root
+define('URLROOT', '_YOUR_URL_');
+// Site name
+define('SITENAME', '_YOUR_SITENAME_');
---
+++ app/libraries/Controller.php
@@ -0,0 +1,26 @@
+<?php
+// 
+// Base controller
+// Loads the models and views
+// 
+class Controller {
+  // Load model
+  public function model($model) {
+    // Require model file
+    require_once '../app/models/' . $model . '.php';
+
+    // Instantiate model
+    return new $model();
+  }
+
+  // Load view
+  public function view($view, $data = []) {
+    // CHeck for the view file
+    if(file_exists('../app/views/' . $view . '.php')) {
+      require_once '../app/views/' . $view . '.php';
+    } else{
+      // View doesn't exist
+      die('View does not exist');
+    }
+  }
+}
